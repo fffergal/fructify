ec84b5aac2070767eb716fbe7936f47adaf79828
Fergal Hainey <fergal+git@bfot.co.uk>
2019-01-16 17:17:02 +0000

    Add JSON logs and Dropbox log export

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,47 @@
 import json
+import logging
+import os
+import time
 import traceback
 import urllib2
 import urlparse
 from contextlib import closing
 from datetime import datetime
+
+
+LOGS_DIR = "logs"
+
+
+class JSONLogFormatter(logging.Formatter):
+
+    def __init__(self, datefmt=None):
+        super(JSONLogFormatter, self).__init__(datefmt=datefmt)
+
+    def format(self, record):
+        attributes = {
+            key: value for key, value in vars(record).items()
+            if isinstance(value, (str, int, float))
+        }
+        try:
+            attributes["message"] = record.getMessage()
+        except Exception:
+            pass
+        try:
+            attributes["asctime"] = self.formatTime(record, datefmt=self.datefmt)
+        except Exception:
+            pass
+        try:
+            attributes["exc_type"] = record.exc_info[0].__name__
+            attributes["traceback"] = "".join(
+                traceback.format_exception(
+                    record.exc_info[0],
+                    record.exc_info[1],
+                    record.exc_info[2]
+                )
+            )
+        except Exception:
+            pass
+        return json.dumps(attributes)
 
 
 def wedding(environ, start_response):
@@ -30,6 +68,20 @@ def dropbox_debug(environ, start_response):
         json.dumps(
             {"value1": json.dumps(urlparse.parse_qs(environ["QUERY_STRING"]), indent=2)}
         ),
+        {"Content-type": "application/json"},
+    )
+    with closing(urllib2.urlopen(request)) as response:
+        lines = list(response.readlines())
+    start_response("200 OK", [("Content-type", "text/plain")])
+    return lines
+
+
+def dropbox_log_route(environ, start_response):
+    with closing(open(os.path.join(LOGS_DIR, "passenger.log"))) as log_file:
+        log_content = log_file.read()
+    request = urllib2.Request(
+        "https://maker.ifttt.com/trigger/dropbox-debug/with/key/dnaJW0wSYg5wScT5JZi-_o",
+        json.dumps({"value1": log_content}),
         {"Content-type": "application/json"},
     )
     with closing(urllib2.urlopen(request)) as response:
@@ -69,11 +121,17 @@ def days_until_route(environ, start_response):
     return lines
 
 
+def error_debug_route(environ, start_response):
+    raise Exception
+
+
 routes = {
     "/v1/wedding": wedding,
     "/v1/debug": debug,
     "/v1/dropbox-debug": dropbox_debug,
     "/v1/days-until": days_until_route,
+    "/v1/error-debug": error_debug_route,
+    "/v1/dropbox-log": dropbox_log_route,
 }
 
 
@@ -85,8 +143,33 @@ def application(environ, start_response):
     try:
         return route(environ, start_response)
     except Exception:
+        try:
+            if not os.path.exists(LOGS_DIR):
+                os.makedirs(LOGS_DIR, 0700)
+            logger = logging.getLogger("fergalsiftttwebhooks")
+            if not logger.handlers:
+                handler = logging.handlers.TimedRotatingFileHandler(
+                    os.path.join(LOGS_DIR, "passenger.log"),
+                    when="D",
+                    interval=1,
+                    utc=True
+                )
+                formatter = JSONLogFormatter()
+                formatter.converter = time.gmtime
+                handler.setFormatter(formatter)
+                logger.addHandler(handler)
+            logger.exception(
+                " - ".join([environ["PATH_INFO"], environ["REQUEST_METHOD"]])
+            )
+        except Exception:
+            start_response("500 Server error", [("Content-type", "text/plain")])
+            return [
+                "Server error\n",
+                "Problem logging error.\n",
+                traceback.format_exc(),
+            ]
         start_response("500 Server error", [("Content-type", "text/plain")])
-        return ["Server error\n", traceback.format_exc()]
+        return ["Server error\n", "Errors logged."]
 
 
 def days_until(date_from, date_to):
--- test_passenger_wsgi.py
+++ test_passenger_wsgi.py
@@ -1,5 +1,11 @@
-import passenger_wsgi
+import json
+import logging
+import time
 from datetime import datetime
+
+import pytest
+
+import passenger_wsgi
 
 
 def test_days_until():
@@ -7,3 +13,21 @@ def test_days_until():
         passenger_wsgi.days_until(datetime(2018, 1, 13, 6, 0), datetime(2018, 1, 15)) ==
         2
     )
+
+
+@pytest.fixture
+def logger(tmpdir):
+    logger = logging.getLogger("testlogger")
+    handler = logging.FileHandler(str(tmpdir / "log.txt"))
+    formatter = passenger_wsgi.JSONLogFormatter()
+    formatter.converter = time.gmtime
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    yield logger
+    logger.removeHandler(handler)
+    handler.close()
+
+
+def test_jsonlogformatter(logger, tmpdir):
+    logger.error("test %s", "yo")
+    assert json.load(tmpdir / "log.txt")["message"] == "test yo"
