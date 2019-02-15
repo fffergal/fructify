9ad2c51eed31aae6efb5d42d913c6d340cf40377
Fergal Hainey <fergal+git@bfot.co.uk>
2019-02-15 22:07:56 +0000

    Add tests and local run using wsgiref

--- test_passenger_wsgi.py
+++ test_passenger_wsgi.py
@@ -1,6 +1,10 @@
 import json
 import logging
+import threading
 import time
+import urllib2
+import wsgiref.simple_server
+from contextlib import closing
 from datetime import datetime
 
 import pytest
@@ -31,3 +35,26 @@ def logger(tmpdir):
 def test_jsonlogformatter(logger, tmpdir):
     logger.error("test %s", "yo")
     assert json.load(tmpdir / "log.txt")["message"] == "test yo"
+
+
+@pytest.fixture
+def server():
+    app_server = wsgiref.simple_server.make_server(
+        "127.0.0.1",
+        8000,
+        passenger_wsgi.application,
+    )
+    serve_thread = threading.Thread(target=app_server.serve_forever)
+    serve_thread.start()
+    yield
+    app_server.shutdown()
+    serve_thread.join(1.0)
+    if serve_thread.is_alive():
+        raise Exception("Server thread could not be stopped.")
+    app_server.server_close()
+
+
+def test_debug(server):
+    response = urllib2.urlopen("http://127.0.0.1:8000/v1/debug?hey=yo", None, 1)
+    with closing(response) as response_body:
+        assert json.loads(unicode(response_body.read(), "utf-8")) == {"hey": ["yo"]}
