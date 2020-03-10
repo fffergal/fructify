0b9ecd571c80be4edcbe9bc841c9034ef15a9514
Fergal Hainey <fergal+git@bfot.co.uk>
2020-03-10 23:53:25 +0000

    Init beeline just before first request

--- fructify/tracing.py
+++ fructify/tracing.py
@@ -1,5 +1,6 @@
 import atexit
 import os
+import threading
 
 import beeline
 from beeline.middleware.bottle import HoneyWSGIMiddleware
@@ -7,10 +8,24 @@ from beeline.middleware.bottle import HoneyWSGIMiddleware
 
 def with_tracing(app):
     """Add tracing to a WSGI app."""
-    beeline.init(
-        writekey=os.environ["HONEYCOMB_KEY"],
-        dataset="IFTTT webhooks",
-        service_name="fructify",
-    )
-    atexit.register(beeline.close)
-    return HoneyWSGIMiddleware(app)
+
+    def traced_init_app(environ, start_response):
+        # Import time could happen before forking, so init beeline just before first
+        # response.
+        with beeline_init_lock:
+            if not with_tracing.beeline_inited:
+                beeline.init(
+                    writekey=os.environ["HONEYCOMB_KEY"],
+                    dataset="IFTTT webhooks",
+                    service_name="fructify",
+                    debug=True,
+                )
+                atexit.register(beeline.close)
+                with_tracing.beeline_inited = True
+
+        return app(environ, start_response)
+    return HoneyWSGIMiddleware(traced_init_app)
+
+
+with_tracing.beeline_inited = False
+beeline_init_lock = threading.Lock()
