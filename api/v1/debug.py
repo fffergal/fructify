1c68777805bed295dc7f49de57dffaf07f06224d
Fergal Hainey <fergal+git@bfot.co.uk>
2020-03-13 18:34:50 +0000

    Fix debug response to be bytes
    To be compatible with wsgiref when testing.

--- api/v1/debug.py
+++ api/v1/debug.py
@@ -9,7 +9,12 @@ from fructify.tracing import with_tracing
 def app(environ, start_response):
     if environ["REQUEST_METHOD"] != "POST":
         start_response("200 OK", [("Content-type", "application/json")])
-        return [json.dumps(urllib.parse.parse_qs(environ["QUERY_STRING"]), indent=2)]
+        return [
+            bytes(
+                json.dumps(urllib.parse.parse_qs(environ["QUERY_STRING"]), indent=2),
+                "utf-8",
+            )
+        ]
     with contextlib.closing(environ["wsgi.input"]) as request_body_file:
         request_body = request_body_file.read(int(environ["CONTENT_LENGTH"]))
     start_response("200 OK", [("Content-type", "text/plain")])
