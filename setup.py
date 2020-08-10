d736bf4b81edb90903047ba895564eda4f0e426a
Fergal Hainey <fergal+git@bfot.co.uk>
2020-08-10 00:38:45 +0000

    Add backports.zoneinfo to dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages=find_packages(exclude=["tests.*"]),
     install_requires=[
         "authlib~=0.14",
+        "backports.zoneinfo~=0.2",
         "blinker~=1.4",
         "flask~=1.1",
         "honeycomb-beeline~=2.11",
